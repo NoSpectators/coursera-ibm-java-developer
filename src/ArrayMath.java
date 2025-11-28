0a8cad4336ded58df827499c9c962e1f71f110d4
NoSpectators <john.c.erickson@gmail.com>
2025-11-28 18:07:56 -0500

    operators

--- src/ArrayMath.java
+++ src/ArrayMath.java
@@ -2,9 +2,9 @@ public class ArrayMath {
     public static void main(String []args) {
 	
         int[] arr = {30, 20, 100, 95, 1, -10, 8, 2000, 9, 878};
-	System.out.println("arr.length = " + arr.length);
+        System.out.println("arr.length = " + arr.length);
         
-	// addition
+        // addition
         System.out.println("arr[0] - arr[1] = " + (arr[0] - arr[1]));
 
         // subtraction
@@ -23,7 +23,7 @@ public class ArrayMath {
         for (int i = 0; i < arr.length-1; i++) {
             // ternary operator
             String greater = (arr[i] > arr[i+1]) ? "greater" : "not greater!";
-	    System.out.println(arr[i] + " > " + arr[i+1] + " ?? " + greater);
+            System.out.println(arr[i] + " > " + arr[i+1] + " ?? " + greater);
         }
     }
 } 
