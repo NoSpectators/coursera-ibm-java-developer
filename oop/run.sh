a90cd21175638ba24933c11406e036b8c82ebd7e
NoSpectators <john.c.erickson@gmail.com>
2025-12-09 09:44:36 -0500

    initial effort

--- oop/run.sh
+++ oop/run.sh
@@ -2,3 +2,8 @@
 
 export CLASSPATH=$CLASSPATH:/home/john/my-projects/coursera-ibm-java-developer/oop/classes
 
+#javac -d classes/ src/Book.java src/BookAccess.java
+#java BookAccess
+
+javac -d classes/ src/BooksMenu.java
+java BooksMenu
