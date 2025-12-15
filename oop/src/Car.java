c55774070bd2ee122ccfecbe3ccf50667830eec1
NoSpectators <john.c.erickson@gmail.com>
2025-12-15 11:36:15 -0500

    implemented private method in Car class

--- oop/src/Car.java
+++ oop/src/Car.java
@@ -1,13 +1,48 @@
 public class Car {
     // note: nothing is static here. every car object instatiated
     // on the Car class might have differing values. 
-    String color;
+    private String color; // color is private, so it can only be accessed within the Car class
     String model;
-    int year;
+    private int year; // year is private, so it can only be accessed within the Car class
 
+    // a static variable keeps track of the total number of Car objects created. 
+    // this value is static, meaning it's shared with all instances of the Car class.
+    // this can be thought of a "global variable" bound to the Car class.
+    static int numberOfCars = 0;
+
+    // default constructor
+    public Car() {
+        numberOfCars++; // increment this number each time Car is instantiated.
+    }
+
+
+    // private access modifier means this method is ONLY accessible within the class
+    // this means this method is not accessable outside of the class
+    // called below in displayInfo()
+    private void displayCount() {
+        System.out.println("Total Cars " + numberOfCars);
+    }
+    
+    // since color is private, it needs a setter
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // since year is private, it needs a setter
+    public void setYear(int year) {
+        this.year = year;
+    }
+    
     void displayInfo() {
         System.out.println("Car Model: " + model);
         System.out.println("Car Color: " + color);
         System.out.println("Car Year: " + year);
+        this.displayCount();
+        // this.displayColor();
+    }
+    // the private access modifier ensures this method can only be called 
+    // within the Car class
+    private void displayColor() {
+        System.out.println("Car color: " + this.color);
     }
 }
--- oop/src/CarMenu.java
+++ oop/src/CarMenu.java
@@ -3,11 +3,25 @@ public class CarMenu {
     public static void main(String[] args) {
         // create an object of the Car class 
         Car myCar = new Car();
-        myCar.color = "Red";
+        // myCar.color = "Red"; // color is Private to it needs to be set using setColor("Red");
+        myCar.setColor("Red");
         myCar.model = "Toyota";
-        myCar.year = 2020;
+        // myCar.year = 2020; // year is Private, so it needs to be set using setYear(2020)
+        myCar.setYear(2020);
     
         // display car info by calling the displayInfo() method
         myCar.displayInfo();
+
+	// so far, there should be only 1 Car created. 
+        // create another car and note now there are 2 cars.
+        Car myCar2 = new Car();
+        myCar2.setColor("Blue");
+        myCar2.model = "Nissan";
+        myCar2.setYear(2000);
+        
+        myCar2.displayInfo();
+       
+        // this should not work
+        // myCar2.displayColor(); // correct it doesn't work
     }
 }
