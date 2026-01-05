871ffa55f979d36693fb9d1d343f4cdac1eeb9a5
NoSpectators <john.c.erickson@gmail.com>
2026-01-05 13:47:49 -0500

    spacing

--- fileio/src/ReadFileExampleWithScanner.java
+++ fileio/src/ReadFileExampleWithScanner.java
@@ -7,7 +7,7 @@ class ReadFileExampleWithScanner {
         try {
             Scanner scanner = new Scanner(System.in);
             // FileReader looks for the directory of where the terminal is running the file (if only filename given).
-	    System.out.println("Enter the name of the file you want to read.");
+            System.out.println("Enter the name of the file you want to read.");
             Scanner fileScanner = new Scanner(new FileReader(scanner.nextLine()));
             while (fileScanner.hasNext()) {
                 String fileLine = fileScanner.nextLine();
