4b8a124ef8a855902c26dc451cb623c727ba9d44
NoSpectators <john.c.erickson@gmail.com>
2026-01-05 13:46:29 -0500

    part1 of lab

---
+++ fileio/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export CLASSPATH=$CLASSPATH:/home/john/my-projects/coursera-ibm-java-developer/fileio/classes
+
+
+javac -d classes src/ReadFileExampleWithScanner.java 
+java ReadFileExampleWithScanner 
---
+++ fileio/src/ReadFileExampleWithScanner.java
@@ -0,0 +1,23 @@
+import java.io.FileReader; // character stream
+import java.util.Scanner; // user input
+import java.io.FileNotFoundException; 
+
+class ReadFileExampleWithScanner {
+    public static void main(String[] args) {
+        try {
+            Scanner scanner = new Scanner(System.in);
+            // FileReader looks for the directory of where the terminal is running the file (if only filename given).
+	    System.out.println("Enter the name of the file you want to read.");
+            Scanner fileScanner = new Scanner(new FileReader(scanner.nextLine()));
+            while (fileScanner.hasNext()) {
+                String fileLine = fileScanner.nextLine();
+                System.out.println(fileLine);
+            }
+            // close the scanner
+            fileScanner.close();
+        } catch(FileNotFoundException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
+
