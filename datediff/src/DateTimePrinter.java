3ff72662f5f8b41abd4566015653d75401679d14
NoSpectators <john.c.erickson@gmail.com>
2026-01-20 09:15:17 -0500

    saving

--- datediff/src/DateTimePrinter.java
+++ datediff/src/DateTimePrinter.java
@@ -7,18 +7,18 @@ import java.util.Scanner;
 public class DateTimePrinter {
     public static void main(String[] args) {
         // prompt user to enter preferred date format
-	LocalDate todayDate = LocalDate.now();
+        LocalDate todayDate = LocalDate.now();
         Scanner scanner = new Scanner(System.in);
-	System.out.println("Enter the format you would like to print the date in\n" + 
+        System.out.println("Enter the format you would like to print the date in\n" + 
                            "dd for date, \n" + 
                            "M for month, MM for zero-padded month, MMM for abbreviated month, MMMM for full name, \n" + 
                            "yy or yyyy for year");
-	String strDateFormat = scanner.nextLine();
-	DateTimeFormatter newdateformat = DateTimeFormatter.ofPattern(strDateFormat);
-	System.out.println("\nThe date is " + todayDate.format(newdateformat)); 
+        String strDateFormat = scanner.nextLine();
+        DateTimeFormatter newdateformat = DateTimeFormatter.ofPattern(strDateFormat);
+        System.out.println("\nThe date is " + todayDate.format(newdateformat)); 
         
         // prompt user to enter the preferred time format
-	LocalTime nowTime = LocalTime.now();
+        LocalTime nowTime = LocalTime.now();
         System.out.println("\nEnter the format you would like to print the time in\n" + 
         "H for Hour of day (0-23), HH for Zero-padded hour of day (00-23), \n" + 
         "h for Hour of am/pm (1-12), hh for Zero-padded hour of am/pm (01-12) \n" + 
